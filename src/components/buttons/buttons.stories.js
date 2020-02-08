22952b74c31b401765237479f2e486a3340ce89b
Burton Smith <bsmith@amazazing.com>
2020-02-08 08:41:26 -0500

    added boilerplate code

--- src/components/buttons/buttons.stories.js
+++ src/components/buttons/buttons.stories.js
@@ -1,9 +1,14 @@
-import { text, select, boolean, button } from '@storybook/addon-knobs';
+import { text, select, boolean } from '@storybook/addon-knobs';
 
 export default { title: 'Button' };
 
 const types = ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'link', 'none'];
 
 export const playground = () => {
+    const type = select('type', types, 'primary');
+    const src = text('src', '');
+    const hollow = boolean('hollow', false);
+    const clear = boolean('clear', false);
+
     return (``)
 }
